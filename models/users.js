8d87ef7d9d0379b04b4585edb2c3d4ca3568d915
AJSevillano <correosevi@gmail.com>
2021-12-11 20:16:57 +0000

    all the models working

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ import users from '../data/users.js';
 
 // GET ALL USERS
 export function getUsers() {
-  //Return the users
+  //Return all the users
   return users;
 }
 
@@ -10,7 +10,7 @@ export function getUsers() {
 export function getUserByID(id) {
   //Find the user with the id given in the params
   const userById = users.find((user) => user.id === id);
-  //Return the user object
+  //Return the user object for the response
   return userById;
 }
 
@@ -20,29 +20,34 @@ export function createUser(newUser) {
   users.push(newUser);
   //Find the new user in the array
   const lastUserAdded = users[users.length - 1];
-  //Return the new created user
+  //Return the new created user for the response
   return lastUserAdded;
 }
 
 // UPDATE A USER BY ID
 export function updateUserByID(id, updatedUser) {
-  // Find the position of the item
+  //Find the position of the user by id
   const foundItem = users.findIndex((user) => user.id === id);
-  //Change the value of the item
+  //Update the value of the item
   users[foundItem] = updatedUser;
-  //Debug
-  console.log(users);
-  //Return the item
+  //Return the user for the response
   return users[foundItem];
 }
 
-//Test the function
-updateUserByID(3, {
-  first_name: 'Manolo',
-  last_name: 'Turri',
-  email: 'kturri2@washington.edu',
-  catchphrase: 'User-centric multimedia collaboration',
-});
-
 // DELETE A USER BY ID
-export function deleteUserByID(id) {}
+export function deleteUserByID(id) {
+  //Find the position of the user by id
+  const foundIndex = users.findIndex((user) => user.id === id);
+  //Store the user in a variable
+  const user = users[foundIndex];
+  //Remove the user from the data
+  users.splice(foundIndex, 1);
+  //Debug
+  console.log(user);
+  console.log(users);
+  //Return the user for the response
+  return user;
+}
+
+//Test the function
+deleteUserByID(21);
