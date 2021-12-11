aa8f834ee3fb5277481f6ebf69fb2e0f98a8c233
AJSevillano <correosevi@gmail.com>
2021-12-11 19:47:53 +0000

    Get functions working on models/users

--- models/users.js
+++ models/users.js
@@ -1,10 +1,20 @@
-import users from "../data/users.js";
+import users from '../data/users.js';
 
 // GET ALL USERS
-export function getUsers() {}
+export function getUsers() {
+  //Return the users
+  return users;
+}
 
 // GET A USER BY ID
-export function getUserByID(id) {}
+export function getUserByID(id) {
+  //Find the user with the id given in the params
+  const userById = users.find((user) => user.id === id);
+  //Return the user object
+  return userById;
+}
+
+getUserByID(1);
 
 // CREATE A USER
 export function createUser(newUser) {}
