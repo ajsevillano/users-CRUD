2c70c534c9a0f79fbf0315c6004b42a62cd9a66f
Sevi <correosevi@gmail.com>
2021-12-15 11:21:08 +0000

    Tidy code up

--- frontEnd/public/main.js
+++ frontEnd/public/main.js
@@ -82,36 +82,61 @@ function createRow(id, firstName, lastName, email, catchphrase) {
 }
 
 async function deleteUser(id) {
+  const response = await fetchDelete(id);
+  //Create an animation to remove a row from the DOM when is deleted.
+  removeRowAnimation(id);
+  //Create an alert to confirm user has been deleted
+  createAlert(response);
+}
+
+function createAlert(response) {
+  //Create an alert to show user has been delete.
+  let deleteAlert = document.createElement('div');
+  deleteAlert.innerText = `User ${response.payload.first_name} has been deleted`;
+  deleteAlert.classList.add('delete-warning');
+  container.appendChild(deleteAlert);
+  //We run an animation to show and hide the Alert
+  alertAnimation(deleteAlert);
+  //Remove the deleteAlert alert from the DOM after 0.6sec
+  removeAlert(deleteAlert);
+}
+
+//Remove the deleteAlert alert from the DOM after 0.6sec
+function removeAlert(deleteAlert) {
+  setTimeout(() => {
+    container.removeChild(deleteAlert);
+  }, 1600);
+}
+
+//ANIMATIONS
+function alertAnimation(deleteAlert) {
+  //After show the deleteAlert wait and move the deleteAlert down
+  setTimeout(() => {
+    deleteAlert.classList.add('slide-down-animation');
+  }, 10);
+  //After 1.5s hide the deleteAlert up
+  setTimeout(() => {
+    deleteAlert.classList.add('slide-up-animation');
+  }, 1500);
+}
+
+function removeRowAnimation(id) {
   const row = document.querySelector(`#row-${id}`);
+  //Hide the row with an Fade out animation
+  row.classList.add('hidden');
+  //Remove the row from the DOM after 0.5sec
+  setTimeout(() => {
+    tableRows.removeChild(row);
+  }, 500);
+}
+
+//FETCH FUNCTIONS
+async function fetchDelete(id) {
   //We send a DELETE fetch and wait for the response
   const fetchResponse = await fetch(`http://localhost:3000/users/${id}`, {
     method: 'DELETE',
   });
   //Store the response.
   const response = await fetchResponse.json();
-  //Hide the row with an Fade out animation
-  row.classList.add('hidden');
-  //Remove the row from the DOM after 0.5sec
-  setTimeout(() => {
-    tableRows.removeChild(row);
-  }, 450);
-  //Create an alert to show user has been delete.
-  let deleteWarning = document.createElement('div');
-  deleteWarning.innerText = `User ${response.payload.first_name} has been deleted`;
-  deleteWarning.classList.add('delete-warning');
-  container.appendChild(deleteWarning);
-  //After show the deleteWarning wait and move the deleteWarning down
-  setTimeout(() => {
-    deleteWarning.classList.add('slide-down-animation');
-  }, 10);
-  //After 1.5s hide the deleteWarning up
-  setTimeout(() => {
-    deleteWarning.classList.add('slide-up-animation');
-  }, 1500);
-  //Remove the deleteWarning alert from the DOM after 0.6sec
-  setTimeout(() => {
-    container.removeChild(deleteWarning);
-  }, 1600);
-
-  // alert(`The user ${response.payload.first_name} has been deleted`);
+  return response;
 }
