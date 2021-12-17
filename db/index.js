06efc7085f9070d922f8c5d355364d7a8fdb60a3
Sevi <correosevi@gmail.com>
2021-12-17 20:44:23 +0000

    Install dotenv and pg packages

---
+++ db/index.js
@@ -0,0 +1,15 @@
+import pg from 'pg';
+import dbConfig from '../dbConfig.js';
+
+const pool = new pg.Pool({
+  user: dbConfig.user,
+  host: dbConfig.host,
+  database: dbConfig.database,
+  password: dbConfig.password,
+  port: dbConfig.port,
+  ssl: { rejectUnauthorized: false },
+});
+
+export default function query(text, params) {
+  return pool.query(text, params);
+}
