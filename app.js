6c35b16507e02f4552eb560ed1bda034b8e93336
Sevi <correosevi@gmail.com>
2021-12-20 09:09:54 +0000

    Create routes folder, move routes from app to the folder

--- app.js
+++ app.js
@@ -1,72 +1,18 @@
 import express from 'express';
 import { html } from './config.js';
-import {
-  getUserByID,
-  getUsers,
-  createUser,
-  updateUserByID,
-  deleteUserByID,
-} from './models/users.js';
+import router from './routes/users.js';
+
 const PORT = 3000;
 const app = express();
 
 app.use(express.static('frontEnd/public'));
 app.use(express.static('frontEnd/libs'));
 app.use(express.json());
+app.use('/', router);
 
 /** DO NOT CHANGE THIS ROUTE - it serves our front-end */
 app.get('/', function (req, res) {
   res.sendFile(html);
-});
-
-// GET ALL USERS
-app.get('/users', async function (req, res) {
-  //Call the function to get the users
-  const result = await getUsers();
-  //Return the response
-  res.json(result);
-});
-
-// GET USER BY ID
-app.get(`/users/:id`, async function (req, res) {
-  //Get the id from the params
-  const id = req.params.id;
-  //Call the function to get the user by Id
-  const resultbyId = await getUserByID(id);
-  //Return the response
-  res.json(resultbyId);
-});
-
-// CREATE AN USER
-app.post(`/users`, async function (req, res) {
-  //Get the body from the params
-  const postBody = req.body;
-  //Call the function to add the new user
-  const newUser = await createUser(postBody);
-  //Return the response
-  res.json(newUser);
-});
-
-//UPDATE AN USER BY ID
-app.put('/users/:id', async function (req, res) {
-  //Get the body from the params
-  const body = req.body;
-  //Get the id
-  const id = req.params.id;
-  //Call the function to add the new user
-  const updateUser = await updateUserByID(id, body);
-  //Return the response
-  res.json(updateUser);
-});
-
-//DELETE AN USER BY ID
-app.delete('/users/:id', async function (req, res) {
-  //Get the id
-  const id = req.params.id;
-  //Call the function to add the new user
-  const deleteUser = await deleteUserByID(id);
-  //Return the response
-  res.json(deleteUser);
 });
 
 app.listen(PORT, () => {
---
+++ routes/users.js
@@ -0,0 +1,61 @@
+import express from 'express';
+const router = express.Router();
+import {
+  getUserByID,
+  getUsers,
+  createUser,
+  updateUserByID,
+  deleteUserByID,
+} from '../models/users.js';
+
+// GET ALL USERS
+router.get('/users', async function (req, res) {
+  //Call the function to get the users
+  const result = await getUsers();
+  //Return the response
+  res.json(result);
+});
+
+// GET USER BY ID
+router.get(`/users/:id`, async function (req, res) {
+  //Get the id from the params
+  const id = req.params.id;
+  //Call the function to get the user by Id
+  const resultbyId = await getUserByID(id);
+  //Return the response
+  res.json(resultbyId);
+});
+
+// CREATE AN USER
+router.post(`/users`, async function (req, res) {
+  //Get the body from the params
+  const postBody = req.body;
+  //Call the function to add the new user
+  const newUser = await createUser(postBody);
+  //Return the response
+  res.json(newUser);
+});
+
+//UPDATE AN USER BY ID
+router.put('/users/:id', async function (req, res) {
+  //Get the body from the params
+  const body = req.body;
+  //Get the id
+  const id = req.params.id;
+  //Call the function to add the new user
+  const updateUser = await updateUserByID(id, body);
+  //Return the response
+  res.json(updateUser);
+});
+
+//DELETE AN USER BY ID
+router.delete('/users/:id', async function (req, res) {
+  //Get the id
+  const id = req.params.id;
+  //Call the function to add the new user
+  const deleteUser = await deleteUserByID(id);
+  //Return the response
+  res.json(deleteUser);
+});
+
+export default router;
