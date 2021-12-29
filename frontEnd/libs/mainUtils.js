b43dae8dbaaf6d23f8caeced76c85833e65f7a82
Sevi <correosevi@gmail.com>
2021-12-29 15:14:25 +0000

    Move utils for the main file to an external file

---
+++ frontEnd/libs/mainUtils.js
@@ -0,0 +1,33 @@
+import { getAllElements } from './dom.js';
+import { generateTableRow } from './generateTableRow.js';
+import { updateFormObjectValues } from './newUserHandler.js';
+
+//ORDER THE USERS FECTH BY ID
+export function orderFetchUsersById(payload) {
+  return payload.sort(function (a, b) {
+    return a.id - b.id || a.name.localeCompare(b.name);
+  });
+}
+
+//MAP USER LIST AND GENERATE THE TABLE
+export function mapUsersList(usersListById) {
+  return usersListById.map((user) =>
+    generateTableRow(
+      user.id,
+      user.first_name,
+      user.last_name,
+      user.email,
+      user.catchphrase
+    )
+  );
+}
+
+//CREATE THE EVENT LISTENERS
+export function createEventListeners() {
+  const getAllInputs = getAllElements('.add-user-form-container', 'input');
+  return getAllInputs.forEach((element) => {
+    element.addEventListener('keyup', (e) =>
+      updateFormObjectValues(e, element.id)
+    );
+  });
+}
--- frontEnd/public/main.js
+++ frontEnd/public/main.js
@@ -1,46 +1,16 @@
 import { getUsers } from '../crud.js';
-import { getAllElements } from '../dom.js';
-import { generateTableRow } from '../generateTableRow.js';
-import { updateFormObjectValues } from '../newUserHandler.js';
+import * as mainUtils from '../mainUtils.js';
 
 async function runApp() {
   //GET ALL USERS
   const usersList = await getUsers();
   //ORDER USERS BY ID
-  const usersListById = orderFetchUsersById(usersList.payload);
+  const usersListById = mainUtils.orderFetchUsersById(usersList.payload);
   // CREATE THE TABLE ROWS
-  mapUsersList(usersListById);
+  mainUtils.mapUsersList(usersListById);
   //CREATE EVENT LISTENERS FOR THE INPUTS
-  createEventListeners();
+  mainUtils.createEventListeners();
 }
 
+//START THE APP
 runApp();
-
-function orderFetchUsersById(payload) {
-  return payload.sort(function (a, b) {
-    return a.id - b.id || a.name.localeCompare(b.name);
-  });
-}
-
-//MAP USER LIST AND GENERATE THE TABLE
-function mapUsersList(usersListById) {
-  return usersListById.map((user) =>
-    generateTableRow(
-      user.id,
-      user.first_name,
-      user.last_name,
-      user.email,
-      user.catchphrase
-    )
-  );
-}
-
-//CREATE THE EVENT LISTENERS
-function createEventListeners() {
-  const getAllInputs = getAllElements('.add-user-form-container', 'input');
-  return getAllInputs.forEach((element) => {
-    element.addEventListener('keyup', (e) =>
-      updateFormObjectValues(e, element.id)
-    );
-  });
-}
